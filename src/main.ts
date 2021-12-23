19f2f18fc363506ef539ab6916667d11e25dd8dc
7dev7urandom <7dev7urandom@users.noreply.github.com>
2021-12-23 20:48:26 +0700

    Try an update

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+/* eslint-disable i18n-text/no-en */
 import * as core from '@actions/core'
 import * as gh from '@actions/github'
 // import http from 'http'
@@ -7,24 +8,63 @@ async function run(): Promise<void> {
   try {
     const host: string = core.getInput('hostname')
     const port: string = core.getInput('port')
-    const path: string = core.getInput('path')
+    // const path: string = core.getInput('path')
     const waitTime = Number(core.getInput('queryInterval'))
     const auth: string = core.getInput('auth')
     const timeout: number = Number(core.getInput('timeout')) * 1000
     const startTime = Date.now()
     core.info(`Starting! Job is running`)
     core.debug(
-      `Inputs: webhook: ${host}:${path}:${port}, queryInterval: ${waitTime}, timeout: ${timeout}`
+      `Inputs: webhook: ${host}:${port}, queryInterval: ${waitTime}, timeout: ${timeout}`
     )
+    core.debug(`Sending request at ${new Date().toTimeString()}...`)
+    const form = new FormData()
+    form.append('id', `${gh.context.repo.owner}:${gh.context.repo.repo}`)
+    form.append('secret', auth)
+    await new Promise<void>(r => {
+      form.submit(`http://${host}:${port}/deploy`, (err, res) => {
+        if (err) {
+          if (err.message.includes('ECONNREFUSED')) {
+            r()
+            return
+          }
+          throw err
+        }
+        res.resume()
+        core.debug(`Got response code ${res.statusCode}`)
+        if (res.statusCode === 400) {
+          core.setFailed('The authorization token is invalid')
+        } else if (res.statusCode === 500) {
+          let allData = ''
+          res.on('data', chunk => {
+            allData += chunk
+          })
+          res.on('end', () => {
+            core.setFailed(`Error from server: ${allData}`)
+          })
+        } else if (res.statusCode !== 299 && res.statusCode !== 202) {
+          let allData = ''
+          res.on('data', chunk => {
+            allData += chunk
+          })
+          res.on('end', () => {
+            core.setFailed(
+              `Error unrecognized code ${res.statusCode}: ${allData}`
+            )
+          })
+        }
+        r()
+      })
+    })
     let trying = true
     while (trying) {
       try {
         core.debug(`Sending request at ${new Date().toTimeString()}...`)
-        const form = new FormData()
-        form.append('id', `${gh.context.repo.owner}/${gh.context.repo.repo}`)
-        form.append('secret', auth)
+        const form2 = new FormData()
+        form2.append('id', `${gh.context.repo.owner}:${gh.context.repo.repo}`)
+        form2.append('secret', auth)
         await new Promise<void>(r => {
-          form.submit(`http://${host}:${port}${path}`, (err, res) => {
+          form2.submit(`http://${host}:${port}/status`, (err, res) => {
             if (err) {
               if (err.message.includes('ECONNREFUSED')) {
                 r()
@@ -34,33 +74,29 @@ async function run(): Promise<void> {
             }
             res.resume()
             core.debug(`Got response code ${res.statusCode}`)
-            if (res.statusCode === 200) {
-              trying = false
-            } else if (res.statusCode === 403) {
-              core.setFailed('The authorization token is invalid')
-              trying = false
-            } else if (res.statusCode === 500) {
-              let allData = ''
-              res.on('data', chunk => {
-                allData += chunk
-              })
-              res.on('end', () => {
+            let allData = ''
+            res.on('data', chunk => {
+              allData += chunk
+            })
+            res.on('end', () => {
+              if (allData.toLowerCase().includes('error')) {
+                trying = false
                 core.setFailed(`Error from server: ${allData}`)
-              })
-              trying = false
-            } else if (res.statusCode !== 299 && res.statusCode !== 202) {
-              let allData = ''
-              res.on('data', chunk => {
-                allData += chunk
-              })
-              res.on('end', () => {
-                core.setFailed(
-                  `Error unrecognized code ${res.statusCode}: ${allData}`
-                )
-              })
-              trying = false
-            }
-            r()
+                r()
+                return
+              } else if (allData.toLowerCase().includes('success')) {
+                trying = false
+                core.info(`Success! Job is done`)
+                r()
+                return
+              } else if (!allData.toLowerCase().includes('processing')) {
+                trying = false
+                core.setFailed(`Error unrecognized status: ${allData}`)
+                r()
+                return
+              }
+              r()
+            })
           })
         })
       } catch (e) {
@@ -80,7 +116,7 @@ async function run(): Promise<void> {
       core.setOutput('time', new Date().toTimeString())
     }
   } catch (error) {
-    core.setFailed(error.message)
+    core.setFailed((error as Error).message)
   }
 }
 
